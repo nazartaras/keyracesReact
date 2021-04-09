dfc9b5105f32b767909810c9a5eee07fed542763
Taras <tarasnazar93@gmail.com>
2021-04-09 21:31:41 +0300

    add ts and eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,48 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
-const path = require('path');
-const fs = require('fs');
+const path = require("path");
+const fs = require("fs");
 
 const appDirectory = fs.realpathSync(process.cwd());
 
-const resolveAppPath = relativePath => path.resolve(appDirectory, relativePath);
-const host = process.env.HOST || 'localhost';
+const resolveAppPath = (relativePath) =>
+  path.resolve(appDirectory, relativePath);
+const host = process.env.HOST || "localhost";
 
 const htmlPlugin = new HtmlWebPackPlugin({
-    inject: true,
-    template: resolveAppPath('./src/index.html'),
+  inject: true,
+  template: resolveAppPath("./src/index.html"),
 });
 module.exports = {
-  entry: './src/index.js',
-  output: { 
-    path: path.resolve(__dirname, 'build'), 
-    publicPath: '/',
-    filename: 'bundle.js'
-  }, 
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    publicPath: "/",
+    filename: "bundle.js",
+  },
   plugins: [htmlPlugin],
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      }
-    ]
+        test: /\.(ts|tsx)$/,
+        loader: "awesome-typescript-loader",
+      },
+      {
+        enforce: "pre",
+        test: /\.js$/,
+        loader: "source-map-loader",
+      },
+      {
+        test: /\.css$/,
+        loader: "css-loader",
+      },
+    ],
   },
   devServer: {
-    contentBase: resolveAppPath('public'),
+    contentBase: resolveAppPath("public"),
     compress: true,
     hot: true,
     host,
     port: 3000,
-    publicPath: '/',
+    publicPath: "/",
   },
 };
